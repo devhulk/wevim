4ed66934d8b94abbfd8aafcbb0ca2a1fe2d7735f
devhulk <gerald.yerden@icloud.com>
2017-08-23 18:41:11 -0400

    restructure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: __dirname,
-    filename: './src/js/bundle.js'
+    filename: './src/dist/bundle.js'
   },
   module: {
     loaders: [
